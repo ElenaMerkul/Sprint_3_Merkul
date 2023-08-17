0a2deb316f33109d837814d596bd4399d7c085b3
Elena Merkul <merkul-e@mail.ru>
2023-08-17 21:42:50 +0300

    Added_faker_and_locator

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -1,12 +1,15 @@
+import faker as faker
+from faker import Faker
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from locators import Locators
+from selenium.webdriver.common.by import By
 
+faker = Faker()
 class TestUserRegisration():
-
     def test_user_registration_correct_password_and_login(self, driver):
         name = "Елена"
-        email = "merkulelena122@mail.ru"
+        email = faker.email()
         password = "123456"
 
         driver.find_element(*Locators.BUTTON_LOGIN).click()
@@ -18,12 +21,8 @@ class TestUserRegisration():
         driver.find_element(*Locators.PASSWORD_INPUT).send_keys(password)
         driver.find_element(*Locators.BUTTON_LOGIN).click()
 
-        text = WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((By.XPATH, "//a[@class='Auth_link__1fOlj']"))).text
+        text = WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.LINK_REGISTER)).text
         assert text == "Зарегистрироваться"
-
-        # понимаю, что это одноразовый тест, но в проекте генерация паролей и логинов - это дополнительное и необязательное задание.
-        # В вебинаре посмотрела как добавлять такие функции, но пока не хватает времени
-
 
     def test_user_registration_password_less_than_6_fail(self, driver):
         name = "Елена"
